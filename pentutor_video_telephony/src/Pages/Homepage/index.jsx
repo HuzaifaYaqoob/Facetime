6114bab70311fdf2f883e3bde9508f5ad562719e
HuzaifaYaqoob <zeevhyorg@gmail.com>
2022-06-28 23:13:02 +0500

    socket inilized

--- pentutor_video_telephony/src/Pages/Homepage/index.jsx
+++ pentutor_video_telephony/src/Pages/Homepage/index.jsx
@@ -1,7 +1,22 @@
+import { useEffect } from "react";
 
 
 
 const Homepage = (props) => {
+
+    useEffect(()=>{
+
+        let socket = new WebSocket('ws://192.168.238.107:8000/ws/video-chat/hello/')
+        socket.onopen = (e) =>{
+            console.log(e)
+        }
+        socket.onmessage = (e) =>{
+            console.log(e)
+        }
+        socket.onclose = (e) =>{
+            console.log(e)
+        }
+    } , [])
     return (
         <>
             this is home page
